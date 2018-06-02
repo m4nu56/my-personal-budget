5d28e2f44d23fc7f28cdfda1b68056c37bc231a9
Emmanuel Balpe <e.balpe@dev1-0.com>
2018-06-02 16:43:47 +0200

    feat: login form + budget list
    - add a login form
    - add a static budget list
    - import bootstrap lib
    - create header and footer for logged pages

---
+++ src/BudgetList.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default class BudgetList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+
+        const lstMouvement = [
+            {id: 1, date: new Date(), montant: 123.22, libelle: 'achat bidule'},
+            {id: 1, date: new Date(), montant: 123.22, libelle: 'achat bidule'},
+            {id: 1, date: new Date(), montant: 123.22, libelle: 'achat bidule'},
+            {id: 1, date: new Date(), montant: 123.22, libelle: 'achat bidule'},
+        ];
+        const mouvementRows = lstMouvement.map((mouvement) => {
+            return (
+                <tr>
+                    <td>{mouvement.id}</td>
+                    <td>{mouvement.date.toDateString()}</td>
+                    <td>{mouvement.montant}</td>
+                    <td>{mouvement.libelle}</td>
+                </tr>
+            );
+        });
+
+        return (
+            <table className="table table-sm table-dark">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Montant</th>
+                    <th scope="col">Libelle</th>
+                </tr>
+                </thead>
+                <tbody>
+                {mouvementRows}
+                </tbody>
+            </table>
+        );
+    }
+}
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './footer.css';
+
+export default function Footer(props) {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <span className="text-muted">Place sticky footer content here.</span>
+            </div>
+        </footer>
+    );
+}
