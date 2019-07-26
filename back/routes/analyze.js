a0c0d2ae5ba630cfbbfa0240e21b9e9bcf21ac7d
Emmanuel Balpe <e.balpe@dev1-0.com>
2019-07-26 10:33:27 +0200

    chores: remove back/ from this project and move all front files one folder up

--- back/routes/analyze.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const db = require('./movementQueries');
-const router = express.Router();
-
-router.get('/', db.analyzeMovementByMonthByCategory);
-
-module.exports = router;
