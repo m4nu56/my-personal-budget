b461f06fa563a52dd6c5a96189e35efa657c8374
Emmanuel Balpe <e.balpe@dev1-0.com>
2019-12-09 23:57:32 +0100

    feat: add an import movement button on toolbar of MovementList

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Admin, ListGuesser, Resource } from 'react-admin'
 import dataProvider from './providers/dataProvider'
 import { MovementCard } from './components/movements/movement.card'
 import { CategoryCard } from './components/categories/category.card'
-import { MovementList } from './components/movements/movements.list'
+import {MovementList} from './components/movements/movements.list';
 import Dashboard from './pages/Dashboard'
 import watchAll from './sagas/api-saga'
 import rootReducer from './reducers'
@@ -11,7 +11,7 @@ import rootReducer from './reducers'
 const App = () => <Admin dataProvider={dataProvider} dashboard={Dashboard}
                          customSagas={[watchAll]}
                          customReducers={{ rootReducer }}>
-  <Resource name="movements" list={MovementList} edit={MovementCard}/>
+  <Resource name="movements" list={MovementList} edit={MovementCard} create={MovementCard} />
   <Resource name="categories" list={ListGuesser} edit={CategoryCard}/>
 </Admin>
 
--- src/components/movements/movements.list.js
+++ src/components/movements/movements.list.js
@@ -1,17 +1,59 @@
-import React from 'react'
-import { Datagrid, DateField, List, NumberField, ReferenceField, TextField } from 'react-admin'
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import {CreateButton, Datagrid, DateField, ExportButton, List, NumberField, ReferenceField, TextField} from 'react-admin';
+import Toolbar from '@material-ui/core/Toolbar';
 
-export const MovementList = props => (
-  <List {...props}>
-    <Datagrid rowClick="edit">
-      <TextField source="id"/>
-      <TextField source="year"/>
-      <TextField source="month"/>
-      <DateField source="date"/>
-      <NumberField source="amount"/>
-      <TextField source="label"/>
-      <ReferenceField source="category_id" reference="categories"><TextField source="name"/></ReferenceField>
-      <NumberField source="category.id"/>
-    </Datagrid>
-  </List>
-)
+
+const ListActions = ({
+                         basePath,
+                         currentSort,
+                         displayedFilters,
+                         exporter,
+                         filters,
+                         filterValues,
+                         onUnselectItems,
+                         resource,
+                         selectedIds,
+                         showFilter,
+                         total
+                     }) => {
+
+    console.log(resource)
+
+    return (
+        <Toolbar>
+            {filters && React.cloneElement(filters, {
+                resource,
+                showFilter,
+                displayedFilters,
+                filterValues,
+                context: 'button'
+            })}
+            <CreateButton basePath={basePath}/>
+            <ExportButton
+                disabled={total === 0}
+                resource={resource}
+                sort={currentSort}
+                filter={filterValues}
+                exporter={exporter}
+            />
+            <Button color="primary" onClick={() => console.log('custom action')}>Import movements</Button>
+        </Toolbar>
+    );
+}
+
+export const MovementList = props =>{
+    return (
+    <List {...props} actions={<ListActions />}>
+        <Datagrid rowClick="edit">
+            <TextField source="id"/>
+            <TextField source="year"/>
+            <TextField source="month"/>
+            <DateField source="date"/>
+            <NumberField source="amount"/>
+            <TextField source="label"/>
+            <ReferenceField source="category_id" reference="categories"><TextField source="name"/></ReferenceField>
+            <NumberField source="category.id"/>
+        </Datagrid>
+    </List>
+)};
